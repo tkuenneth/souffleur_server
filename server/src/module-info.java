5870a0a9599969f86e1253a4c5ae3f673f55b6fa
Thomas Kuenneth <kuennetht@gmail.com>
2019-11-05 08:48:25 +0100

    Create module-info.java

---
+++ server/src/module-info.java
@@ -0,0 +1,7 @@
+module com.thomaskuenneth.souffleur.server {
+    requires jdk.httpserver;
+    requires java.desktop;
+    requires java.logging;
+    requires org.json;
+    requires com.google.zxing;
+}
