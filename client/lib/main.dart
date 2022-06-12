8c883cac66df7a354420a434f9e3bcf65fdbb90f
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-06-12 09:06:56 +0200

    Cleaned up the code

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,14 +1,9 @@
 import 'dart:async';
-import 'dart:convert';
-import 'dart:ffi';
 
-import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:swipedetector/swipedetector.dart';
 
 void main() => runApp(SouffleurClient());
 
@@ -18,98 +13,91 @@ class SouffleurClient extends StatefulWidget {
 }
 
 class _SouffleurClientState extends State<SouffleurClient> {
-  var notesGroup = AutoSizeGroup();
+  var lastKnownUrl = "";
 
   @override
   void initState() {
     super.initState();
+    SharedPreferences.getInstance().then((prefs) {
+      setState(() {
+        lastKnownUrl = prefs.getString('lastKnownUrl');
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SwipeDetector(
-      onSwipeLeft: () => setState(() {
-        fetchSlideNotes("next");
-      }),
-      onSwipeRight: () => setState(() {
-        fetchSlideNotes("previous");
-      }),
-      child: Container(
-        decoration: BoxDecoration(color: Colors.white),
-        child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: Padding(
-            padding: EdgeInsets.only(left: 16, right: 16),
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: FutureBuilder<bool>(
-                        future: fetchSlideNotes("next"),
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            return Text("ok");
-                          } else {
-                            if (snapshot.connectionState ==
-                                ConnectionState.done) {
-                              return FlatButton(
-                                child: Text(
-                                  "Scan",
-                                  style: TextStyle(
-                                      fontSize: 72, color: Colors.black),
-                                ),
-                                onPressed: _onPressed,
-                              );
-                            } else {
-                              return CircularProgressIndicator();
-                            }
-                          }
-                        }),
-                  ),
-                ),
-                FutureBuilder<bool>(
-//                    future: currentSlideNotes,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        var slideNotes = snapshot.data;
-                        return Padding(
-                            padding: EdgeInsets.only(bottom: 4),
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Padding(
+          padding: EdgeInsets.only(left: 16, right: 16),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: FutureBuilder<bool>(
+                      future: _getFromServer(lastKnownUrl, "hello"),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState != ConnectionState.done) {
+                          return CircularProgressIndicator();
+                        } else if (snapshot.data == true) {
+                          return Text("ok",
+                              style:
+                                  TextStyle(fontSize: 72, color: Colors.black));
+                        } else {
+                          return TextButton(
                             child: Text(
-                              "super",
-                              style: TextStyle(
-                                  fontSize: 32, color: Colors.black38),
-                            ));
-                      } else {
-                        return SizedBox.shrink();
-                      }
-                    }),
-              ],
-            ),
+                              "Scan",
+                              style:
+                                  TextStyle(fontSize: 72, color: Colors.black),
+                            ),
+                            onPressed: _scanQRCode,
+                          );
+                        }
+                      }),
+                ),
+              ),
+              FutureBuilder<bool>(
+//                    future: currentSlideNotes,
+                  builder: (context, snapshot) {
+                if (snapshot.hasData) {
+//                  var slideNotes = snapshot.data;
+                  return Padding(
+                      padding: EdgeInsets.only(bottom: 4),
+                      child: Text(
+                        "super",
+                        style: TextStyle(fontSize: 32, color: Colors.black38),
+                      ));
+                } else {
+                  return SizedBox.shrink();
+                }
+              }),
+            ],
           ),
         ),
       ),
     );
   }
 
-  Future _onPressed() async {
+  void _scanQRCode() async {
     try {
-      String lastKnownUrl = await BarcodeScanner.scan().toString();
-      SharedPreferences.getInstance().then((prefs) {
-        prefs.setString('lastKnownUrl', lastKnownUrl);
-        setState(() {
-          fetchSlideNotes("start");
-        });
+      var scanResult = await BarcodeScanner.scan();
+      var prefs = await SharedPreferences.getInstance();
+      prefs.setString('lastKnownUrl', scanResult.rawContent);
+      setState(() {
+        lastKnownUrl = scanResult.rawContent;
       });
     } on Exception catch (e) {
       debugPrint('$e');
     }
   }
 
-  Future<bool> fetchSlideNotes(String suffix) async {
+  Future<bool> _getFromServer(String lastKnownUrl, String suffix) async {
     try {
-      final prefs = await SharedPreferences.getInstance();
-      final lastKnownUrl = prefs.getString('lastKnownUrl');
+      if (!lastKnownUrl.contains("http")) return false;
       final response = await http
           .get(Uri.parse(lastKnownUrl + suffix))
           .timeout(const Duration(seconds: 5));
