b5dc91b827a6a298b2f1b20bd78ab1925af9d279
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-06-18 12:24:17 +0200

    Version 1.0.1 finished

--- server/src/eu/thomaskuenneth/souffleur/Main.java
+++ server/src/eu/thomaskuenneth/souffleur/Main.java
@@ -18,7 +18,7 @@ import java.util.logging.Logger;
 
 public class Main extends JFrame {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
@@ -175,6 +175,7 @@ public class Main extends JFrame {
 
     private JPanel createIndicators() {
         JPanel panel = UIFactory.createFlowPanel();
+        panel.setBorder(BorderFactory.createTitledBorder("Command indicators"));
         panel.add(createIndicator(Server.HOME));
         panel.add(createIndicator(Server.PREVIOUS));
         panel.add(createIndicator(Server.NEXT));
