fc992750616bd0233bac7ecb513464eb96e3ce33
Thomas Künneth <thomas@thomaskuenneth.eu>
2022-11-18 16:49:07 +0100

    Documentation updates

--- server/src/jvmMain/java/eu/thomaskuenneth/souffleur/Server.java
+++ server/src/jvmMain/java/eu/thomaskuenneth/souffleur/Server.java
@@ -20,6 +20,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
+/**
+ * The Souffleur server utilizes {@code com.sun.net.httpserver.HttpsServer} to listen to
+ * client requests. It uses a keystore that holds a self-signed certificate. To create a
+ * keystore you can do something like this:
+ * <pre>
+ * keytool -genkeypair -keyalg RSA -alias selfsigned
+ *         -keystore souffleur.jks -storepass password
+ *         -validity 9999 -keysize 2048
+ * </pre>
+ * In this case, the password would be <strong>password</strong>.
+ */
 public class Server implements HttpHandler {
 
     public static final String END = "end";
